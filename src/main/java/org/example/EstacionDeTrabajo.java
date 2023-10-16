196d98af5775605ac84daa4c4038247593be350e
Zara Martín <zmartand@myuax.es>
2023-10-16 11:47:40 +0200

    EstacionDeTrabajo.java

--- src/main/java/org/example/EstacionDeTrabajo.java
+++ src/main/java/org/example/EstacionDeTrabajo.java
@@ -11,8 +11,19 @@ public class EstacionDeTrabajo implements Runnable {
         this.indiceEstacion = indiceEstacion;
     }
 
+
     @Override
     public void run() {
-        
+        try {
+            for (int i = 1; i <= 100; i++) {
+                Componente componente = new Componente(tipoComponente, System.currentTimeMillis());
+                buffer.producir(componente);
+                Thread.sleep(50); // Simulando el tiempo de producción
+
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
     }
 }

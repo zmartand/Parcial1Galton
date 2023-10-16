29b21eb357f9fcf0d71d52a4f7f76fdc2cc5ea3a
Zara Martín <zmartand@myuax.es>
2023-10-16 11:49:46 +0200

    Creacion LineaDeEnsamblaje.java

---
+++ src/main/java/org/example/LineaDeEnsamblaje.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class LineaDeEnsamblaje {
+    private final BufferCompartido buffer;
+
+    public LineaDeEnsamblaje(BufferCompartido buffer) {
+        this.buffer = buffer;
+    }
+    
+}

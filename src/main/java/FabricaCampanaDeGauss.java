e0418464e6311668d9a461b53886df19cc688f5a
Zara Martín <zmartand@myuax.es>
2023-10-16 12:04:31 +0200

    Principal FabricaCampanaDeGauss.java

---
+++ src/main/java/FabricaCampanaDeGauss.java
@@ -0,0 +1,25 @@
+import org.example.BufferCompartido;
+import org.example.EstacionDeTrabajo;
+import org.example.FabricaVisual;
+import org.example.LineaDeEnsamblaje;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class FabricaCampanaDeGauss {
+    private static final int NUM_ESTACIONES = 5;
+
+    public static void main(String[] args) {
+        FabricaVisual visual = new FabricaVisual();
+        BufferCompartido buffer = new BufferCompartido(10);
+
+        String[] tiposComponentes = {"TipoA", "TipoB", "TipoC", "TipoD", "TipoE"};
+        ExecutorService executor = Executors.newFixedThreadPool(NUM_ESTACIONES + 1);
+        for (int i = 0; i < NUM_ESTACIONES; i++) {
+            executor.execute(new EstacionDeTrabajo(buffer, tiposComponentes[i],visual, i ));
+        }
+        executor.execute(new LineaDeEnsamblaje(buffer, visual));
+        executor.shutdown();
+
+    }
+}

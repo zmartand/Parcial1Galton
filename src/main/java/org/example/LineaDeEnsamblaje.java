8915be3a063836145c4feb713138a2d2db81acc4
Zara Martín <zmartand@myuax.es>
2023-10-16 11:53:14 +0200

    LineaDeEnsamblaje.java

--- src/main/java/org/example/LineaDeEnsamblaje.java
+++ src/main/java/org/example/LineaDeEnsamblaje.java
@@ -9,6 +9,24 @@ public class LineaDeEnsamblaje implements Runnable{
 
     @Override
     public void run() {
-        
+        try{
+            for (int i = 1; i <= 100; i++) {
+                Componente componente = buffer.consumir();
+                ensamblarMaquina(); // Método para ensamblar la máquina
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private void ensamblarMaquina(){
+        //Simular el ensamblaje de la máquina con los componentes
+        System.out.println("Ensamblaje de la máquina enprogreso...");
+        try {
+            Thread.sleep(5000); //Simular el tiempo de ensamblaje
+        }catch (InterruptedException e){
+            Thread.currentThread().interrupt();
+        }
+        System.out.println("Máquina ensamblada exitosamente!!");
     }
 }

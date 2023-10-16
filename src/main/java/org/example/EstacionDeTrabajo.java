69195abdacadf3e1ad68745d7e0cf0167103f09c
Zara Martín <zmartand@myuax.es>
2023-10-16 11:58:13 +0200

    Estacion de trabajo + FabricaVisual

--- src/main/java/org/example/EstacionDeTrabajo.java
+++ src/main/java/org/example/EstacionDeTrabajo.java
@@ -4,13 +4,15 @@ public class EstacionDeTrabajo implements Runnable {
     private final BufferCompartido buffer;
     private final String tipoComponente;
     private final int indiceEstacion;
+    private final FabricaVisual visual;
 
-    public EstacionDeTrabajo(BufferCompartido buffer, String tipoComponente, int indiceEstacion) {
+
+    public EstacionDeTrabajo(BufferCompartido buffer, String tipoComponente, int indiceEstacion, FabricaVisual visual) {
         this.buffer = buffer;
         this.tipoComponente = tipoComponente;
         this.indiceEstacion = indiceEstacion;
+        this.visual = visual;
     }
-
 
     @Override
     public void run() {
@@ -19,6 +21,7 @@ public class EstacionDeTrabajo implements Runnable {
                 Componente componente = new Componente(tipoComponente, System.currentTimeMillis());
                 buffer.producir(componente);
                 Thread.sleep(50); // Simulando el tiempo de producción
+                visual.actualizarProduccion(indiceEstacion, i);
 
             }
         } catch (InterruptedException e) {

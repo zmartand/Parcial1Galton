5f69bc351b9541e949536b6c6eda49a3dc2cc41f
Zara Martín <zmartand@myuax.es>
2023-10-16 11:50:24 +0200

    LineaDeEnsamblaje.java implementa interfaz Runnable

--- src/main/java/org/example/LineaDeEnsamblaje.java
+++ src/main/java/org/example/LineaDeEnsamblaje.java
@@ -1,10 +1,14 @@
 package org.example;
 
-public class LineaDeEnsamblaje {
+public class LineaDeEnsamblaje implements Runnable{
     private final BufferCompartido buffer;
 
     public LineaDeEnsamblaje(BufferCompartido buffer) {
         this.buffer = buffer;
     }
-    
+
+    @Override
+    public void run() {
+        
+    }
 }
